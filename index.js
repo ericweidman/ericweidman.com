f13b92a0a7d90997a9847648c0eb9e6cbed8df8e
ericweidman <ericweidman@gmail.com>
2017-01-04 00:10:08 -0500

    fixed

---
+++ index.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+    $("#page").addClass("animated zoomInDown");
+});
+$(document).ready(function() {
+    $("#this").hover(function() {
+        $("#this").addClass("animated bounce");
+    });
+});
+$(document).ready(function() {
+    $("#this1").hover(function() {
+        $("#this1").addClass("animated bounce");
+    });
+});
+$(document).ready(function() {
+    $("#this2").hover(function() {
+        $("#this2").addClass("animated bounce");
+    });
+});
+$(document).ready(function() {
+    $("#fixedheader").addClass("animated rollIn");
+});
+$(document).ready(function() {
+    $("#fixedfooter").addClass("animated rollIn");
+});
+$(document).ready(function() {
+    $("#b1").click(function() {
+        $("body").addClass("animated hinge");
+        setTimeout(function() {
+            window.location.replace("https://github.com/ericweidman");
+        }, 1700)
+    })
+  });
