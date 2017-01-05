6f25c78f66bb7035e94249c9548d2b1f457dbb18
ericweidman <ericweidman@gmail.com>
2017-01-05 00:41:07 -0500

    1.0

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
+
+
+
 $(document).ready(function() {
-    $("#page").addClass("animated zoomInDown");
+    $("#page").addClass("animated fadeIn");
 });
 $(document).ready(function() {
     $("#this").hover(function() {
@@ -17,7 +21,7 @@ $(document).ready(function() {
     });
 });
 $(document).ready(function() {
-    $("#fixedheader").addClass("animated rollIn");
+    $("#fixedheader").addClass("animated slideInDown");
 });
 // $(document).ready(function() {
 //     $("#fixedfooter").addClass("animated rollIn");
@@ -38,9 +42,34 @@ $(document).ready(function() {
 var waypoint = new Waypoint({
   element: document.getElementById('butts'),
   handler: function() {
-     $("#b1").addClass("animated bounceIn");
-     $("#b2").addClass("animated bounceIn");
-     $("#b3").addClass("animated bounceIn");
+     $("#b1").addClass("animated zoomIn");
+     $("#b2").addClass("animated zoomIn");
+     $("#b3").addClass("animated zoomIn");
+  },
+  offset: 850
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('stuff'),
+  handler: function() {
+     $("#p1").addClass("animated slideInLeft");
+     $("#p2").addClass("animated slideInLeft");
+     $("#p3").addClass("animated slideInLeft");
+     $("#p4").addClass("animated slideInRight");
+     $("#p5").addClass("animated slideInRight");
+     $("#p6").addClass("animated slideInRight");
   },
   offset: 700
 })
+
+function core() {
+  $('html, body').animate({
+      scrollTop: $("#stuff").offset().top - 220},
+      'slow');
+};
+
+function contact() {
+  $('html, body').animate({
+    scrollTop: $("#fixedfooter").offset().top},
+      'slow');
+};
